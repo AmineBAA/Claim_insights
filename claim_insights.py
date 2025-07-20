eb138a8fe232f14a4a78b37cf3a86baef099d12b
Amine BAAZZOUZ <141878518+AmineBAA@users.noreply.github.com>
2025-07-20 16:14:05 +0100

    Update claim_insights.py

--- claim_insights.py
+++ claim_insights.py
@@ -8,7 +8,7 @@ from datetime import datetime
 import numpy as np
 
 
-st.image("logo.png", use_column_width=False)  # image en haut de page
+st.image("logo_saham.png", use_column_width=False)  # image en haut de page
 
 st.set_page_config(page_title="Reporting Réclamations Avancé", layout="wide")
 st.title("📊 Reporting Réclamations - Version Avancée")
